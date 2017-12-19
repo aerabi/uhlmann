fa1e65b624ae2e95421239a289dca00c661d4bb0
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2017-12-19 22:50:11 +0100

    First Django demo

--- arno/arno/settings.py
+++ arno/arno/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '46.101.251.251',
     'server',
+    'localhost',
 ]
 
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'charts',
 ]
 
 MIDDLEWARE = [
--- arno/charts/views.py
+++ arno/charts/views.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+import math
+
+
+def chart(request):
+    context = {
+        'labels': ['%d.12.2017' % day for day in range(1, 13)],
+        'datasets': [
+            {'data': ', '.join([str(math.cos(i)) for i in range(1, 13)]), 'color': 'red'},
+            {'data': ', '.join([str(math.sin(i)) for i in range(1, 13)]), 'color': 'blue'},
+        ]
+    }
+    return render(request, 'charts/chart.html', context)
